a7eaf209a1c8c08f79468e19045ded047ff03ea6
bartsimo <bartke.simon@gmail.com>
2023-02-06 17:28:31 +0100

    Comments for day 4

--- four.py
+++ four.py
@@ -1,11 +1,15 @@
 # read in data
 def read_input(input_file):
+    # f is a file object
     with open(input_file) as f:
+        # first, read() gives a string that is split into a list at line breaks by splitlines.
         data_list = f.read().splitlines()
+        # last element useless, list is changed in-place
         data_list.pop()
 
     lol = []
     for i in data_list:
+        # splits string at , returns result as list
         splits = i.split(",")
         lol.append(splits)
 
@@ -13,17 +17,22 @@ def read_input(input_file):
 
 
 def is_useless(ls: list):
+    # splits string at -, returns tuple
+    # works because of concrete form of str
     min1, max1 = ls[0].split("-")
     min2, max2 = ls[1].split("-")
+    # create one tuple
     ranges = (min1, max1, min2, max2)
+    # cast every tuple element to string
     ranges_int = tuple(map(int, ranges))
+    # either one range is smaller than the other or the other is smaller than one or not
     if ranges_int[0] <= ranges_int[2] and ranges_int[3] <= ranges_int[1] or ranges_int[2] <= ranges_int[0] and ranges_int[1] <= ranges_int[3]:
         return 1
     else:
         return 0
 
     
-
+# simple logic in one main function that calls helpers
 def main():
     result = 0
     lol = (read_input("input4"))
--- four_hard.py
+++ four_hard.py
@@ -1,4 +1,5 @@
-# read in data
+# documentation almost identical to four.py
+
 def read_input(input_file):
     with open(input_file) as f:
         data_list = f.read().splitlines()
@@ -17,6 +18,7 @@ def is_overlap(ls: list):
     min2, max2 = ls[1].split("-")
     ranges = (min1, max1, min2, max2)
     ranges_int = tuple(map(int, ranges))
+    # min of one range is between min and max of other range or vice versa or not
     if ranges_int[0] <= ranges_int[2] <= ranges_int[1] or ranges_int[2] <= ranges_int[0] <= ranges_int[3]:
         return 1
     else:
