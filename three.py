cdaa93fd46911a83e01bd3cdbd677f2dc44bdff7
bartsimo <bartke.simon@gmail.com>
2023-02-06 17:18:53 +0100

    Commented scripts for day 3

--- three.py
+++ three.py
@@ -2,13 +2,14 @@
 # read in data
 def read_input(input_file):
     with open(input_file) as f:
+        # first, read() gives a string that is split into a list at line breaks by splitlines.
         data_list = f.read().splitlines()
 
         return data_list
 
 
 
-# check whether all strings have even length:
+# check whether all strings have even length (just to be sure):
 """
 all_even = True
 for i in data_list:
@@ -20,14 +21,24 @@ print(all_even)
 def get_common_letter(text: str) -> str:
     first_half = text[:(len(text)//2)]
     second_half = text[(len(text)//2):]
+    # this works because we know from task three that there will be one common letter in both halves
     for letter in first_half:
+        # the str built-in function returns -1 when no match is found
         if second_half.find(letter) != -1:
+            # in this case, return this letter and leave loop
             return letter
+    # if no match is found, return a string that is not a part of the dataset for sanity test
+    # in order to hold contract of functions' signature
+    return "$"
+
 
 def get_priority(s: str) -> int:
     if s.islower() == True:
+        # every str can be mapped to an ASCII value e.g. by the built in function ord()
+        # subtracting 96 from lowercase letters ...
         return ord(s) - 96
     else:
+        # ... and 38 from uppercase letters gives correct value according to specification in task 3
         return ord(s) - 38
 
 
--- three_hard.py
+++ three_hard.py
@@ -1,18 +1,23 @@
 
 # read in data
 def read_input(input_file):
+    # return file object
     with open(input_file) as f:
+        # first, read() gives a string that is split into a list at line breaks by splitlines.
         data_list = f.read().splitlines()
 
+        # make sure that dataset is divisible by three
         # print(len(data_list) % 3)
 
         return data_list
 
 def get_groups_of_three(data_list: list) -> list:
+    # create list of list of three entries
     data_list_threes = []
+    # go through dataset
     while len(data_list) > 0:
-    # Ab hier!
         counter = 0
+        # fill a group with three entries and save this group as a list
         group = []
         while counter < 3:
             group.append(data_list.pop())
@@ -27,13 +32,15 @@ def get_groups_of_three(data_list: list) -> list:
 def get_common_letter(group: list) -> str:
     group_of_sets = []
 
+    # save every string as a set (no duplicates but valid since we are ultimately interested in a str of length 1)
     for member in group:
         member_as_set = set(member)
         group_of_sets.append(member_as_set)
 
+    # find intersection of three sets and return the intersection
     return group_of_sets[0].intersection(group_of_sets[1], group_of_sets[2]).pop()
 
-
+# see three.py
 def get_priority(s: str) -> int:
     if s.islower() == True:
         return ord(s) - 96
@@ -41,7 +48,7 @@ def get_priority(s: str) -> int:
         return ord(s) - 38
 
 
-
+# run programm with one main function that calls helper functions
 def main():
     data_list = read_input("input3")
     data_list_threes = get_groups_of_three(data_list)
